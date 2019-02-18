defbfcb483f73c21a69736e31973aab7e201e80d
spro77 <spro77@gmail.com>
2019-02-19 00:51:30 +0200

    Data parsing method added

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 func cross(A []string, B []string) []string {
 	var C []string
 	for _, a := range A {
@@ -25,9 +29,11 @@ var (
 	cols   = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
 	digits = "123456789"
 
-	squares  = cross(rows, cols) // The Array of all cell's names (81)
-	unitlist = [][]string{}      // The Array of Cols, Rows and Boxes (9 column units, 9 row units and 9 box units)
-	units    = map[string][][]string{}
+	squares  = cross(rows, cols)       // The Array of all cell's names (81)
+	unitlist = [][]string{}            // The Array of Cols, Rows and Boxes (9 column units, 9 row units and 9 box units)
+	units    = map[string][][]string{} // Units (Value) per Square (Key)
+
+	peers = map[string][]string{} // Peers, dependent squares (Value) per Square (Key)
 
 	nassigns      = 0
 	neliminations = 0
@@ -35,6 +41,8 @@ var (
 )
 
 func main() {
+
+	//___________________________________ Creating Unitlist by concatenating all units
 
 	for _, c := range cols {
 		unitlist = append(unitlist, cross(rows, []string{c}))
@@ -53,6 +61,8 @@ func main() {
 		}
 	}
 
+	//___________________________________
+
 	for _, s := range squares {
 
 		units[s] = [][]string{}
@@ -64,8 +74,48 @@ func main() {
 		}
 	}
 
-	//fmt.Println(units)
-	//fmt.Println(units["C2"])
+	//___________________________________
+
+	for _, s := range squares {
+
+		set := []string{}
+
+		for _, unit := range units[s] {
+			for _, square := range unit {
+				if square != s {
+					set = append(set, square)
+				}
+			}
+		}
+		peers[s] = set
+	}
+
+	//___________________________________
+
+	fmt.Println(parseGrid(examp))
+
+	//fmt.Println("All cell peers", peers["I9"])
+	//fmt.Println("All cell units", units["C2"])
 	//fmt.Printf("len=%d cap=%d %v\n", len(squares), cap(squares), squares)
 
+}
+
+func parseGrid(grid string) map[string]string {
+	symbols := digits + ".0"
+	gridChars := []string{}
+	m := make(map[string]string)
+
+	for _, char := range grid {
+		for _, b := range symbols {
+			if char == b {
+				gridChars = append(gridChars, string(char))
+			}
+		}
+	}
+
+	for i := 0; i < len(gridChars); i++ {
+		m[squares[i]] = gridChars[i]
+	}
+
+	return m
 }
